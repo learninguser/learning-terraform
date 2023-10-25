2e7963a51b7e8cb499f755475ca4d0b577b967ae
Pavan Kumar Akula <akulapavankumar@outlook.com>
2023-10-25 16:27:17 +0200

    Add block for creating Route53 records

--- 04-loops-route53/ec2.tf
+++ 04-loops-route53/ec2.tf
@@ -8,3 +8,13 @@ resource "aws_instance" "web" {
     Name = var.instance_names[count.index]
   }
 }
+
+resource "aws_route53_record" "record" {
+  count   = length(var.instance_names)
+  zone_id = var.zone_id
+  # interpolation
+  name    = "${var.instance_names[count.index]}.${var.domain_name}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.web[count.index].private_ip]
+}
